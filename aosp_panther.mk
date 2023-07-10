82e1b3e8fa1b0df04210cc894aab872c1adc86c4
Kitsunejasutin <justineryandeguia@yahoo.com>
2023-07-10 13:00:29 +0800

    Rename standard AOSP makefiles  * This is to use aosp_codename makefiles for lunch command to work

--- aosp_panther.mk
+++ aosp_panther.mk
@@ -1,32 +1,31 @@
 #
-# Copyright 2021 The Android Open-Source Project
+# Copyright (C) 2022 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
-TARGET_LINUX_KERNEL_VERSION := 5.10
+# Inherit some common AOSP stuff.
+TARGET_DISABLE_EPPE := true
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-USE_SWIFTSHADER := true
-BOARD_USES_SWIFTSHADER := true
+# Inherit device configuration
+$(call inherit-product, device/google/pantah/aosp-g_panther.mk)
+$(call inherit-product, device/google/gs201/custom_common.mk)
 
-$(call inherit-product, device/google/gs201/aosp_common.mk)
-$(call inherit-product, device/google/pantah/device-panther.mk)
+include device/google/pantah/panther/device-custom.mk
 
+# Device identifier. This must come after all inclusions
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 7
 PRODUCT_NAME := aosp_panther
-PRODUCT_DEVICE := panther
-PRODUCT_MODEL := AOSP on Panther
-PRODUCT_BRAND := Android
-PRODUCT_MANUFACTURER := Google
 
-DEVICE_MANIFEST_FILE := \
-	device/google/pantah/manifest.xml
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=panther \
+    PRIVATE_BUILD_DESC="panther-user 13 TQ3A.230605.012 10204971 release-keys"
+
+BUILD_FINGERPRINT := google/panther/panther:13/TQ3A.230605.012/10204971:user/release-keys
+
+$(call inherit-product, vendor/google/panther/panther-vendor.mk)
