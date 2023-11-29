9b7ab48b930a3245a0912bccbd2893e8851fb3ba
Kitsunejasutin <73803767+Kitsunejasutin@users.noreply.github.com>
2023-11-29 10:20:34 +0800

    pantah: Pixelify  * Rename standard AOSP makefiles Ref: 82e1b3e8fa1b0df04210cc894aab872c1adc86c4

--- aosp_panther.mk
+++ aosp_panther.mk
@@ -1,38 +1,32 @@
 #
-# Copyright 2021 The Android Open-Source Project
+# Copyright (C) 2022 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
-TARGET_LINUX_KERNEL_VERSION := 5.10
+# Inherit some common PixelOS stuff.
+TARGET_DISABLE_EPPE := true
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-USE_SWIFTSHADER := true
-BOARD_USES_SWIFTSHADER := true
+# Inherit device configuration
+$(call inherit-product, device/google/pantah/aosp-g_panther.mk)
+$(call inherit-product, device/google/gs201/custom_common.mk)
 
-$(call inherit-product, device/google/gs201/aosp_common.mk)
-$(call inherit-product, device/google/pantah/device-panther.mk)
+include device/google/pantah/panther/device-custom.mk
 
+# Device identifier. This must come after all inclusions
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 7
 PRODUCT_NAME := aosp_panther
-PRODUCT_DEVICE := panther
-PRODUCT_MODEL := AOSP on Panther
-PRODUCT_BRAND := Android
-PRODUCT_MANUFACTURER := Google
 
-DEVICE_MANIFEST_FILE := \
-	device/google/pantah/manifest.xml
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
 
-# Keep the VNDK APEX in /system partition for REL branches as these branches are
-# expected to have stable API/ABI surfaces.
-ifneq (REL,$(PLATFORM_VERSION_CODENAME))
-  PRODUCT_PACKAGES += com.android.vndk.current.on_vendor
-endif
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=panther \
+    PRIVATE_BUILD_DESC="panther-user 14 UP1A.231105.003 11010452 release-keys"
+
+BUILD_FINGERPRINT := google/panther/panther:14/UP1A.231105.003/11010452:user/release-keys
+
+$(call inherit-product, vendor/google/panther/panther-vendor.mk)
--- cheetah/device-custom.mk
+++ cheetah/device-custom.mk
@@ -6,4 +6,4 @@
 
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/cheetah/overlay-lineage
 
-$(call inherit-product, $(LOCAL_PATH)/device-lineage.mk)
+$(call inherit-product, $(LOCAL_PATH)/device-custom.mk)
